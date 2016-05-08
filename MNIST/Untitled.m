6486aabe53df80b9aceec339f50368b738a68bf3
Vladimir Anisimov <vovaani@gmail.com>
2016-05-08 16:26:53 +0300

    Removed big directories

--- MNIST/Untitled.m
+++
@@ -1,6 +0,0 @@
-sphere; h = findobj('Type','surface');
-hemisphere = [zeros(10,56); zeros(28, 14) reshape(imgs(:,1), 28, 28) zeros(28, 14); zeros(10,56)];
-set(h,'CData',flipud(hemisphere),'FaceColor','texturemap')
-colormap(map)
-axis equal
-view([90 0])
--- example.m
+++ example.m
@@ -1,29 +1,33 @@
-%% Example Title
-% Summary of example objective
-
-%% Section 1 Title
-% escription of first code block
-
 add_paths;
 
 N = 64;
 
-minst5 = loadMNISTImages('train-images.idx3-ubyte');
-minst5 = reshape(minst5(:,1), 28, 28);
-margined_minst5 = zeros(N, N);
-margined_minst5(13 : 40, 13 : 40) = minst5;
+% NOTE: The image was created using the following code. 
+% 
+% minst5 = loadMNISTImages('train-images.idx3-ubyte');
+% minst5 = reshape(minst5(:,1), 28, 28);
+% margined_mnist5 = zeros(N, N);
+% margined_mnist5(13 : 40, 13 : 40) = minst5;
+load margined_mnist5
 
-plot_projection_on_sphere(margined_minst5);
+plot_projection_on_sphere(margined_mnist5);
 
-[sh_pic, sh_dirs, proj_indecies] = project_on_sphere(margined_minst5);
-sh_fourier = directSHT(N, sh_pic, sh_dirs, 'complex');
-
-filt_opt = default_filter_options('dyadic', 2 * N);
-filt_opt.Q = 1;
-filt_opt.B = 0.5;
-filt_opt.boundary = 'nonsymm';
-
-filters = morlet_filter_bank_1d(N, filt_opt);
+% NOTE:
+%     The filters where created by the following code. Currently I've saved
+%     them to .mat files and loading them, becuase I didn't want to upload 
+%     huge amounts of data (the external packages I've used) to git, since
+%     my connection on the satellite internet is quite slow.
+%
+% [sh_pic, sh_dirs, proj_indecies] = project_on_sphere(margined_mnist5);
+% sh_fourier = directSHT(N, sh_pic, sh_dirs, 'complex');
+% 
+% filt_opt = default_filter_options('dyadic', 2 * N);
+% filt_opt.Q = 1;
+% filt_opt.B = 0.5;
+% filt_opt.boundary = 'nonsymm';
+% 
+% filters = morlet_filter_bank_1d(N, filt_opt);
+load filters
 
 figure;
 hold on;
@@ -36,13 +40,13 @@ hold off;
 for i = 1:numel(filters.psi.filter)
     filter = filters.psi.filter{i};
     sh_filtered = apply_filter(filter, sh_fourier, sh_dirs);
-    filtered = sphere_to_grid(size(margined_minst5), sh_filtered, proj_indecies);
+    filtered = sphere_to_grid(size(margined_mnist5), sh_filtered, proj_indecies);
     figure;
     plot_projection_on_sphere(abs(filtered));
 end
 
 filter = filters.phi.filter;
 sh_filtered = apply_filter(filter, sh_fourier, sh_dirs);
-filtered = sphere_to_grid(size(margined_minst5), sh_filtered, proj_indecies);
+filtered = sphere_to_grid(size(margined_mnist5), sh_filtered, proj_indecies);
 figure;
 plot_projection_on_sphere(abs(filtered));
