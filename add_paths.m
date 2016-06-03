86c5f84bed8ac8abb7a65fdb240fa473775d8ecd
Vladimir Anisimov <vovaani@gmail.com>
2016-06-03 15:15:26 +0000

    Fixed pahts

--- add_paths.m
+++ add_paths.m
@@ -1,5 +1,6 @@
 path_to_proj = fileparts(mfilename('fullpath'));
 
+addpath(fullfile(path_to_proj, 'MNIST'));
 addpath(fullfile(path_to_proj, 'polarch-Spherical-Harmonic-Transform-8360a11'));
 addpath(fullfile(path_to_proj, 'scatnet'));
 addpath_scatnet;
